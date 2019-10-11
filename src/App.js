c9165c2813d0ea193149e26713db9292ee7cc318
Mridula Zaman <mridulazaman@Mridulas-MacBook-Air.local>
2019-10-10 20:11:05 -0700

    card-list

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: 'a1'
-        },
-        {
-          name: 'Dracula',
-          id: 'a2'
-        },
-        {
-          name: 'Zombie',
-          id: 'a3'
-        }
-      ]
+      monsters: []
     }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters : users}));
   }
 
   render() {
     return (
       <div className="App">
       {
-        this.state.monsters.map(monster => <h1>{monster.name}</h1>)
+        this.state.monsters.map(monster => <h1 key={monster.id}> {monster.name} </h1> )
       }
       </div>
     );
