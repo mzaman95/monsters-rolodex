2f585fa4683e949c7704c96d859638014cbcd22f
Mridula Zaman <mridula.zaman95@gmail.com>
2019-10-12 14:53:51 -0700

    Updates

---
+++ precache-manifest.77506e268966d1c3e9e15b88f5c8d535.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b35b5548fa08f80dbd71a617013d319d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cb62359e418a142abcb1",
+    "url": "/monsters-rolodex/static/css/main.30e0d275.chunk.css"
+  },
+  {
+    "revision": "ca903edc950d52328219",
+    "url": "/monsters-rolodex/static/js/2.e124cbd2.chunk.js"
+  },
+  {
+    "revision": "cb62359e418a142abcb1",
+    "url": "/monsters-rolodex/static/js/main.999805fd.chunk.js"
+  },
+  {
+    "revision": "d71975295fe45a6cbab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.4686dfee.js"
+  }
+]);
